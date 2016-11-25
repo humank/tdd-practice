58bb26341b56f3517fb9cd9be90f0d3717e7f337
Kim Kao <yikaikao@gmail.com>
2016-11-25 19:08:22 +0800

    Start to practice TDD daily.

---
+++ src/main/java/solid/kim/adapter/BudgetRepository.java
@@ -0,0 +1,18 @@
+package solid.kim.adapter;
+
+import java.util.List;
+
+/**
+ * Created by kim on 2016/11/25.
+ */
+public class BudgetRepository {
+
+    public long counts(){
+        return 1;
+    }
+
+    public List list() {
+
+        return null;
+    }
+}
---
+++ src/main/java/solid/kim/domain/Budget.java
@@ -0,0 +1,9 @@
+package solid.kim.domain;
+
+/**
+ * Created by kim on 2016/11/25.
+ */
+public class Budget {
+    public void add() {
+    }
+}
